401af4c73ddd23932b5c6dba01088393094fcbec
kevin_avot <kevin_avot@ens.univ-artois.fr>
2022-01-19 12:56:10 +0100

    TDD3 done

--- src/main/java/vvl/lisp/LispImpl.java
+++ src/main/java/vvl/lisp/LispImpl.java
@@ -2,6 +2,8 @@ package vvl.lisp;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import vvl.util.ConsList;
 import vvl.util.ConsListFactory;
@@ -10,43 +12,47 @@ public class LispImpl implements Lisp {
 
 	@Override
 	public Object parse(String expr) throws LispError {
+		if (!expr.contains("(")) return getType(expr);
+		
 		expr = expr.replaceAll("\\(", "( ");
 		expr = expr.replaceAll("\\)", " )");
+		expr = expr.trim();
 		String[] parsed = expr.split(" +");
-		// solution 1 : parser normalement puis utiliser asList
-		// solution 2 : remplir consList pendant le parsing
+		
 		ArrayList<ConsList<Object>> consLists = new ArrayList<ConsList<Object>>();
 		consLists.add(ConsListFactory.nil());
 		int consListScope = 0;
 		boolean first = true;
 		Object o = null;
+		
 		for (String string : parsed) {
 			if (!first) {
 				o = getType(string);
-				if (string.equals("(")) {
-					if (consLists.size() >= consListScope++) consLists.add(ConsListFactory.singleton(o));
-					else consLists.set(++consListScope, ConsListFactory.singleton(o));
+				if (string.contains("(")) {
+					if (consLists.size() <= ++consListScope) consLists.add(ConsListFactory.nil());
+					else consLists.set(consListScope, ConsListFactory.nil());
 				}
-				else if (string.equals(")")) {
+				else if (string.contains(")")) {
 					consLists.get(consListScope--).append(o);
 					if (consListScope >= 0)
-						consLists.set(consListScope, consLists.get(consListScope).append(consLists.get(consListScope)));
+						consLists.set(consListScope, consLists.get(consListScope).append(consLists.get(consListScope + 1)));
 				}
 				else {
 					consLists.set(consListScope, consLists.get(consListScope).append(o));
 				}
 			}
-			System.out.println("Lisp : " + string);
 			first = false;
 		}
-		System.out.println(consLists.get(0).toString()); // need to fix lisp with multiple (
 		return consLists.get(0);
 	}
 
 	private Object getType(String string) {
 		Object o = string;
+		Pattern p = Pattern.compile("[0-9]+");
+		Matcher m = p.matcher(string);
 		if (string.contains(".")) o = Double.valueOf(string);
 		else if (string.contains("#")) o = string.contains("t") ? LispBoolean.TRUE : LispBoolean.FALSE;
+		else if (m.matches()) o = BigInteger.valueOf(Long.valueOf(string));
 		return o;
 	}
 
